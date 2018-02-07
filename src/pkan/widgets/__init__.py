7c74b737db8f33a9a9bff93664a0e17835c0ac99
Thomas Massmann <thomas.massmann@it-spir.it>
2018-02-07 07:19:18 +0100

    Fix imports.

--- src/pkan/widgets/__init__.py
+++ src/pkan/widgets/__init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
 """Widgets for PKAN."""
-
-from pkan.widgets.relateditems.widget import RelatedItemsFieldWidget
-
-
-assert RelatedItemsFieldWidget
--- src/pkan/widgets/relateditems/__init__.py
+++ src/pkan/widgets/relateditems/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 """A custom related items widget with add-functionality."""
+
+from pkan.widgets.relateditems.widget import RelatedItemsFieldWidget
+
+
+assert RelatedItemsFieldWidget
