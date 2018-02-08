f1ccd9ec760facc7eb2c65a1282229da804dede1
Thomas Massmann <thomas.massmann@it-spir.it>
2018-02-08 07:19:15 +0100

    Refactoring: extract add-functionality from widget into mixin class.

---
+++ src/pkan/widgets/base.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Base widget classes."""
+
+from plone import api
+from plone.dexterity.interfaces import IDexterityFTI
+from Products.CMFPlone.resources import add_resource_on_request
+from zope.component import getUtility
+from zope.i18n import translate
+
+
+MARKUP = u"""
+{widget}
+<div class="pkan-addnew">
+  <a class="{klass} pat-plone-modal" target="_blank" href="{url}">{title}</a>
+</div>
+"""
+
+
+class AddItemMixin(object):
+    """Add a new item."""
+
+    content_type = None
+    content_type_title = None
+    initial_path = None
+
+    @property
+    def portal_type_name(self):
+        if self.content_type_title:
+            return self.content_type_title
+
+        if not self.content_type:
+            return
+        fti = getUtility(IDexterityFTI, name=self.content_type)
+        return fti.title
+
+    @property
+    def add_url(self):
+        initial_path = self.initial_path
+        if initial_path is None:
+            initial_path = ''
+        else:
+            if initial_path.startswith('/'):
+                initial_path = initial_path[1:]
+            ctx = api.portal.get_navigation_root(self.context)
+            base = ctx.absolute_url()
+            initial_path = '/'.join([base, initial_path])
+        return u'{0}++add++{1}'.format(
+            initial_path,
+            self.content_type,
+        )
+
+    def render_widget(self, widget):
+        add_resource_on_request(self.request, 'pkanwidgets')
+        title = translate(
+            'heading_add_item',
+            domain='plone',
+            mapping={'itemtype': self.portal_type_name},
+            context=self.request,
+            default='Add ${itemtype}',
+        )
+        return MARKUP.format(
+            klass=u'context',
+            title=title,
+            url=self.add_url,
+            widget=widget,
+        )
